b39572ca0d128d4357fbc0318c7fc625eb2ff443
Mario Romera <mariobraojos@icloud.com>
2025-10-06 12:43:53 +0200

    ejercicio6.py

---
+++ clase06-10-25/ejercicio6.py
@@ -0,0 +1,3 @@
+n=int(input("Introduce un entero positivo: "))
+suma = n * (n + 1) // 2
+print("La suma de 1 hasta " + str(n) + " es " + str(suma) )
--- clase06-10-25/ejerciciosdatossimple.py
+++ clase06-10-25/ejerciciosdatossimple.py
@@ -4,9 +4,7 @@
 
 
 #ejercicio6
-n=int(input("Introduce un entero positivo: "))
-suma = n * (n + 1) // 2
-print("La suma de 1 hasta " + str(n) + " es " + str(suma) )
+
 
 #ejercicio7
 peso=float(input("Introduce tu peso (en kg): "))
