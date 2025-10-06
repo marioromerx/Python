b47b34fa582c58d926b264577ceaed0a569ee7a3
Mario Romera <mariobraojos@icloud.com>
2025-10-06 12:43:07 +0200

    ejercicio5

---
+++ clase06-10-25/ejercicio5.py
@@ -0,0 +1,4 @@
+horastrabajadas = float(input("Introduce el número de horas trabajadas: "))
+costehora = float(input("Introduce el coste por hora: "))
+total = costehora*horastrabajadas
+print("El total de la paga que te corresponde es de " + str(total) + " €")
--- clase06-10-25/ejerciciosdatossimple.py
+++ clase06-10-25/ejerciciosdatossimple.py
@@ -1,10 +1,7 @@
 
 
 #ejercicio5
-horastrabajadas = float(input("Introduce el número de horas trabajadas: "))
-costehora = float(input("Introduce el coste por hora: "))
-total = costehora*horastrabajadas
-print("El total de la paga que te corresponde es de " + str(total) + " €")
+
 
 #ejercicio6
 n=int(input("Introduce un entero positivo: "))
