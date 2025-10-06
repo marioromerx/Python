382a09a2accae21fa3e04ad224de60dc470ad3be
Mario Romera <mariobraojos@icloud.com>
2025-10-06 12:39:21 +0200

    ejercicio1.py

--- clase06-10-25/ejerciciosdatossimple.py
+++ clase06-10-25/ejerciciosdatossimple.py
@@ -1,5 +1,5 @@
 #ejercicio1
-print("Hola Mundo")
+
 
 #ejercicio2
 holamundo = "Hola Mundo"
@@ -24,9 +24,13 @@ n=int(input("Introduce un entero positivo: "))
 suma = n * (n + 1) // 2
 print("La suma de 1 hasta " + str(n) + " es " + str(suma) )
 
+#ejercicio7
+peso=float(input("Introduce tu peso (en kg): "))
+altura=float(input("Introduce tu altura (en metros): "))
 
+imc= peso/(altura ** 2)
 
-
+print ("Tu indice de masa corporal es " + str(imc))
 
 
 
