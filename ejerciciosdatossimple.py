a8d7630d7bc95e777c66f91f61cb44c81cd3b296
Mario Romera <mariobraojos@icloud.com>
2025-10-06 12:22:16 +0200

    ejercicio5

--- ejerciciosdatossimple.py
+++ ejerciciosdatossimple.py
@@ -12,3 +12,10 @@ print("Hola " + nombre + "!")
 #ejercicio4 
 operacion = ((3+2) / (2*5)) ** 2
 print(operacion)
+
+#ejercicio5
+horastrabajadas = float(input("Introduce el número de horas trabajadas: "))
+costehora = float(input("Introduce el coste por hora: "))
+total = costehora*horastrabajadas
+print("El total de la paga que te corresponde es de " + str(total) + " €")
+
