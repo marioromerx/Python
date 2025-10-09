599ffbafc217cea9b2eb565d207fe95d38b46fee
Mario Romera <mariobraojos@icloud.com>
2025-10-09 10:12:01 +0200

    ejercicio2.py

---
+++ clase09-10-25/cadenas/ejercicio2.py
@@ -0,0 +1,4 @@
+nombre=input("Introduce tu nombre completo como quieras(mayus, minus..): ")
+print("Tu nombre en minúsculas: "+nombre.lower())
+print("Tu nombre en mayúsculas: "+nombre.upper())
+print("Tu nombre con la primera letra en mayúscula: "+nombre.title())
