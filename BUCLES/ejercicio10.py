8bc0e21ac3ca413b60a5897d853778494dd6742a
Mario Romera <mariobraojos@icloud.com>
2025-11-06 10:27:26 +0100

    deleted

--- BUCLES/ejercicio10.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Introduce un número entero positivo mayor que 2: "))
-i = 2
-while n % i != 0:
-    i = i+1
-if i == n:
-    print(str(n) + " es primo")
-else:
-    print(str(n) + " no es primo")
--- BUCLES/ejercicio9.py
+++
@@ -1,6 +0,0 @@
-CONTRASENA = "contraseña"
-passwd = ""
-while passwd.lower() != CONTRASENA.lower():
-    passwd = input("Introduce la contraseña: ")
-
-print ("Contraseña correcta")
